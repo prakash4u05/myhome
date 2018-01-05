8deb2f1b876ae692f3ba1c09ec132a231a5cdf26
prakash4u05 <prakash4u05@gmail.com>
2018-01-05 15:37:32 +0530

    Add files via upload

---
+++ StreamNewTest.java
@@ -0,0 +1,37 @@
+package com.threadtest;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamNewTest {
+
+	public static void main(String args[]) {
+
+		String fileName = "/Users/Talent/Desktop/pkp.doc";
+		List<String> list = new ArrayList<>();
+
+		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+			//1. filter line 3
+			//2. convert all content to upper case
+			//3. convert it into a List
+			list = stream
+					.filter(line -> !line.startsWith("P"))
+					.map(String::toUpperCase)
+					.collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		list.forEach(System.out::println);
+
+	}
+
+}
